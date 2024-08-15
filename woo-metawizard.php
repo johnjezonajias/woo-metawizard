7bb1e0b0ca66cec35495de703cb83c6567bd6c5c
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-08-15 21:43:37 +0800

    Initial plugin

---
+++ woo-metawizard.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Woo Meta Helper
+ *
+ * @package           WooMetaWizard
+ * @developer         John Jezon Ajias
+ * @license           CC BY-NC 4.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Woo Meta Helper
+ * Plugin URI:        https://webdevjohn.one/woo-meta-helper
+ * Description:       A plugin to enhance WooCommerce meta handling, providing additional meta data management and customization options.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      8.1
+ * Author:            John Jezon Ajias
+ * Author URI:        https://webdevjohn.one
+ * Text Domain:       woo-metawizard
+ * License:           CC BY-NC 4.0
+ * License URI:       https://creativecommons.org/licenses/by-nc/4.0/
+ * Update URI:        https://webdevjohn.one/woo-meta-helper/update
+ * Requires Plugins:  woocommerce, wordpress-seo
+ */
